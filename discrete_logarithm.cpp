4aa391fb5408ca5bc3bbb598898e6e08e20c2b12
Yana541 <149325426+Yana541@users.noreply.github.com>
2024-03-11 15:17:54 +0200

    Create main cpp file

---
+++ discrete_logarithm.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include<map>
+
+using namespace std;
+
+int powmod(int a, int b, int m)
+{
+	int res = 1;
+	while (b > 0)
+		if (b & 1)
+		{
+			res = (res * a) % m;
+			--b;
+		}
+		else
+		{
+			a = (a * a) % m;
+			b >>= 1;
+		}
+	return res % m;
+}
+
+int solve(int a, int b, int m)
+{
+	int n = (int)sqrt(m + .0) + 1;
+	map<int, int> vals;
+	for (int i = n; i >= 1; --i)
+		vals[powmod(a, i * n, m)] = i;
+	for (int i = 0; i <= n; ++i) 
+	{
+		int cur = (powmod(a, i, m) * b) % m;
+		if (vals.count(cur)) 
+		{
+			int ans = vals[cur] * n - i;
+			if (ans < m)
+				return ans;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	int a, b, m;
+	cin >> a >> b >> m;
+	cout << solve(a, b, m);
+	return 0;
+}
+
